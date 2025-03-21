1febb3ab91868d331bb1c80d80e62911b4ba75d2
Miguel Rodríguez Pérez <miguelrp@ext.inditex.com>
2025-03-21 23:39:51 +0100

    [main] added styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import 'react-resizable/css/styles.css';
 import './App.css';
 import { Grid } from './components/grid/grid';
 import { Controls } from './components/controls/controls';
+import { Hero } from './components/hero/hero';
 
 const App = () => {
   return (
     <div className="app-container">
+      <Hero />
       <Controls />
       <Grid />
+      <div className="sphere sphere-2 radial-gradient"></div>
+      <div className="sphere sphere-3 radial-gradient"></div>
     </div>
   );
 };
--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -2,8 +2,13 @@ import './hero.css';
 
 export const Hero = () => {
   return (
-    <div className="hero">
-      <h1>Grid Generator</h1>
-    </div>
+    <section className="hero-section">
+      <p className="title">Tesella</p>
+      <span className="subtitle">
+        Tesella is a CSS and Tailwind grid generator tool that helps developers
+        create custom Tailwind grid layouts more easily.
+      </span>
+      <div className="sphere sphere-1 radial-gradient"></div>
+    </section>
   );
 };
